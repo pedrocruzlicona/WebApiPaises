0678a815b1c893253ac7d7ee55043b4ecc6b4919
Pedro Cruz <pedrocruzmx97@outlook.com>
2019-05-01 21:09:07 -0500

    Se agrega la clase ApplicationDbContext para trabajar con Entity Framework

---
+++ WebApiPaises/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPaises.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        //para crear la tabla de paises
+       public DbSet<Pais> Paises { get; set; }
+    }
+}
